8f8081413dcdcf6f126e96418ed7a4b7a3476774
J8ke-gang <thibaultjake95@gmail.com>
2024-11-14 16:00:28 -0500

    first steps to making page

---
+++ index.js
@@ -0,0 +1,11 @@
+// rock paper scissors
+
+const choices = ["rock","paper","scissors"];
+const playerdisplay = document.getElementById("playerdisplay");
+const computerdisplay = document.getElementById("computerdisplay");
+const resultdisplay = document.getElementById("resultdisplay");
+
+function playgame(playerchoice){
+
+    const computerchoice = choices[Math.floor(math.random() * 3)];
+}
