f5b7630a7ed8ca9ee83d69dacc69822b331d08a5
J8ke-gang <thibaultjake95@gmail.com>
2024-11-15 14:04:05 -0500

    FINSHED RPC

--- index.js
+++ index.js
@@ -4,8 +4,50 @@ const choices = ["rock","paper","scissors"];
 const playerdisplay = document.getElementById("playerdisplay");
 const computerdisplay = document.getElementById("computerdisplay");
 const resultdisplay = document.getElementById("resultdisplay");
+const playerscoredisplay = document.getElementById("playerscoredisplay");
+const computerscoredisplay = document.getElementById("computerscoredisplay");
+let playerscore =0;
+let computerscore = 0;
+
 
 function playgame(playerchoice){
 
-    const computerchoice = choices[Math.floor(math.random() * 3)];
-}
+    const computerchoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerchoice === computerchoice){
+        result = "ITS A TIE";
+    }
+    else{
+        switch(playerchoice){
+            case "rock":
+            result = (computerchoice === "scissors") ? "YOU WIN": "YOU LOSE";
+            break;
+        case "paper":
+            result = (computerchoice === "rock") ? "YOU WIN": "YOU LOSE";
+            break;
+        case "scissors":
+            result = (computerchoice === "paper") ? "YOU WIN": "YOU LOSE";
+            break;
+        }
+    }
+
+    playerdisplay.textContent = `YOU: ${playerchoice}`;
+    computerdisplay.textContent = `COMPUTER: ${computerchoice}`;
+    resultdisplay.textContent = result;
+
+    resultdisplay.classList.remove ("greentext","redtext");
+
+    switch(result){
+        case "YOU WIN":
+            playerscore++;
+            playerscoredisplay.textContent = playerscore;
+        resultdisplay.classList.add("greentext");
+        break;
+        case "YOU LOSE":
+            computerscore++;
+            computerscoredisplay.textContent = computerscore
+        resultdisplay.classList.add("redtext");
+        break;
+    }
+    }
